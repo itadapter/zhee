7ecaaa7b6e1832fba78bb1d651b485fc1e3dcac9
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-04-07 13:27:43 -0400

    stash

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,36 +1,70 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-    entry: {
-        main: './js/main.js'
-        //base: './js/base.js',
-        //string: './js/string.js',
-    },
-    output: {
-      library: "lenin",
-      libraryTarget: "umd",
-      path: path.resolve(__dirname, 'build'),
-      filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
+var cfgBase = {
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
             }
-        ]
-    },
-    resolve: {
-     //root: path.resolve(__dirname, 'js'), 
-     modules: ['js'],
-     extensions: ['*', '.js', '.jsx']
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
+        }
+    ]
+  },
+  resolve: {
+    //root: path.resolve(__dirname, 'js'), 
+    modules: ['js'],
+    extensions: ['*', '.js']
+  },
+  stats: {
+    colors: true
+  },
+  devtool: "source-map"
 };
+
+
+var cfgZhee = {
+  ...cfgBase,
+  entry: {
+    waev: "./zhee/index.js"
+  },
+  output: {
+    library: "zhee",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "../../out/zhee"),
+    filename: "zhee.js" // or [name.js]
+  }
+};
+
+
+var cfgZheeBro = {
+  ...cfgBase,
+  entry: {
+    waev: "./zhee-bro/index.js"
+  },
+  output: {
+    library: "zheebro",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "../../out/zhee-bro"),
+    filename: "zhee-bro.js" // or [name.js]
+  }
+};
+
+var cfgZheeZha = {
+  ...cfgBase,
+  entry: {
+    waev: "./zhee-zha/index.js"
+  },
+  output: {
+    library: "zheezha",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "../../out/zhee-zha"),
+    filename: "zhee-zha.js" // or [name.js]
+  }
+};
+
+
+//Tell Webpack what to zhabify
+module.exports = [ cfgZhee, cfgZheeBro, cfgZheeZha ];
