d81b5831a2d197182048bf8c8a936182e3677f18
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-04-08 18:54:20 -0400

    Add tests

--- src/test/zhee/types.js
+++ src/test/zhee/types.js
@@ -4,6 +4,35 @@ const sut = zhee.Types;
 
 describe("Types", function() {
 
+  describe("#isAssigned()", function() {
+    it("ret false for empty()",   function() { aver.isFalse( sut.isAssigned()           );});
+    it("ret false for undefined", function() { aver.isFalse( sut.isAssigned(undefined)  );});
+    it("ret false for null",      function() { aver.isFalse( sut.isAssigned(null)       );});
+
+    it("ret true for ''",       function() { aver.isTrue( sut.isAssigned("")       );});
+    it("ret true for 'abc'",   function() { aver.isTrue( sut.isAssigned("abc")    );});
+    it("ret true for []",   function() { aver.isTrue( sut.isAssigned([])    );});
+    it("ret true for {}",   function() { aver.isTrue( sut.isAssigned({})    );});
+    it("ret true for true",   function() { aver.isTrue( sut.isAssigned(true)    );});
+    it("ret true for 123",   function() { aver.isTrue( sut.isAssigned(123)    );});
+    
+  }); 
+
+
+  describe("#isString()", function() {
+    it("ret false for empty()",   function() { aver.isFalse( sut.isString()           );});
+    it("ret false for undefined", function() { aver.isFalse( sut.isString(undefined)  );});
+    it("ret false for null",      function() { aver.isFalse( sut.isString(null)       );});
+
+    it("ret false for true",      function() { aver.isFalse( sut.isString(true) );});
+    it("ret false for 1",      function() { aver.isFalse( sut.isString(1)       );});
+    it("ret false for []",      function() { aver.isFalse( sut.isString([])     );});
+    it("ret false for {}",      function() { aver.isFalse( sut.isString({})     );});
+
+    it("ret true for ''",       function() { aver.isTrue( sut.isString("")       );});
+    it("ret true for 'abc'",   function() { aver.isTrue( sut.isString("abc")    );});
+  }); 
+
   describe("#isArray()", function() {
     it("ret false for empty()",   function() { aver.isFalse( sut.isArray()          );});
     it("ret false for undefined", function() { aver.isFalse( sut.isArray(undefined) );});
@@ -16,6 +45,22 @@ describe("Types", function() {
     it("ret true for []",            function() { aver.isTrue( sut.isArray([])           );});
     it("ret true for [null, null]",  function() { aver.isTrue( sut.isArray([null, null]) );});
     it("ret true for [1,2,3]",       function() { aver.isTrue( sut.isArray([1,2,3])      );});
+  });
+
+  describe("#isObject()", function() {
+    it("ret false for empty()",   function() { aver.isFalse( sut.isObject()          );});
+    it("ret false for undefined", function() { aver.isFalse( sut.isObject(undefined) );});
+    it("ret false for null",      function() { aver.isFalse( sut.isObject(null)      );});
+    it("ret false for true",      function() { aver.isFalse( sut.isObject(true)      );});
+    it("ret false for int",       function() { aver.isFalse( sut.isObject(123)       );});
+    it("ret false for string",    function() { aver.isFalse( sut.isObject("zaza")    );});
+
+    it("ret true for {}",        function() { aver.isTrue( sut.isObject({})        );});
+    it("ret true for {a:1...}",  function() { aver.isTrue( sut.isObject({a: 2, b: 3}) );});
+
+    function Car(){ this.a=1; }
+
+    it("ret true for new Car()",  function() { aver.isTrue( sut.isObject(new Car()) );});
   });
 
 });
--- src/zhee/types.js
+++ src/zhee/types.js
@@ -1,31 +1,47 @@
+
+/**
+ * Returns true if the argument is assigned - not undefined non-null value, even empty string is assigned
+ * @param { Object } v
+ */
+export function isAssigned(v) {
+  return v !== undefined && v !== null;//warning:  if (!v) is not the same test!
+}
+
+
 /**
  * Returns true if the argument is a non null string
- * @param { Object } obj
+ * @param { Object } v
  */
-export function isString(obj) {
-  if (obj === undefined) return false;
-  return Object.prototype.toString.call(obj) === "[object String]";
+export function isString(v) {
+  return Object.prototype.toString.call(v) === "[object String]";
 }
 
 /**
  * Returns true when the passed parameter is an array, not a map or function
- * @param { Object } obj
+ * @param { Object } v
  */
-export function isArray(obj) {
-  if (obj === undefined) return false;
+export function isArray(v) {
   return Array.isArray
-    ? Array.isArray(obj)
-    : Object.prototype.toString.call(obj) === "[object Array]";
+    ? Array.isArray(v)
+    : Object.prototype.toString.call(v) === "[object Array]";
 }
 
 /**
- * Returns true when the passed parameter is a map, not an array or function
+ * Returns true when the passed parameter is an object, not an array or function
+ * @param { Object } v
+ */
+export function isObject(v) {
+  return v === Object(v) && !isArray(v) && !isFunction(v);
+}
+
+/**
+ * Returns true when the passed parameter is an array, or map but not a function
  * @param { Object } obj
  */
-export function isObject(obj) {
-  if (obj === undefined) return false;
-  return obj === Object(obj) && !isArray(obj) && !isFunction(obj);
-}
+export function isMapOrArray(obj) {
+  if (obj === undefined || obj === null) return false;
+  return obj === Object(obj) && !isFunction(obj);
+};
 
 /**
  * Returns true when poassed parameter is a function, not a map object or an array
