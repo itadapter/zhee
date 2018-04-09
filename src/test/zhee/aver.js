a89ad194f9bbeed77d015ce1aa3c88cf4e6839c5
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-04-08 20:17:58 -0400

    stash

--- src/test/zhee/aver.js
+++ src/test/zhee/aver.js
@@ -5,12 +5,12 @@ describe("Aver", function() {
 
   describe("#isUndefined()", function() {
 
-    it("ret true for undefined",   function() {
+    it("pass undefined",   function() {
        let x = undefined;
        sut.isUndefined(x);
     });
 
-    it("throw for defined",   function() {
+    it("fail defined",   function() {
       sut.throws(function(){
           let x = 2;
           sut.isUndefined(x);
@@ -20,16 +20,164 @@ describe("Aver", function() {
   
   describe("#isDefined()", function() {
 
-    it("ret true for defined",   function() {
+    it("pass defined",   function() {
        let x = 1;
        sut.isDefined(x);
     });
 
-    it("throw for undefined",   function() {
+    it("fail undefined",   function() {
       sut.throws(function(){
           let x = undefined;
           sut.isDefined(x);
         }, "averment failure: isdefined");
+    });
+  });
+
+  describe("#isNull()", function() {
+
+    it("pass null",   function() {
+       let x = null;
+       sut.isNull(x);
+    });
+
+    it("fail undefined",   function() {
+      sut.throws(function(){
+          let x = undefined;
+          sut.isNull(x);
+        }, "averment failure: isnull");
+    });
+
+    it("fail non null",   function() {
+      sut.throws(function(){
+          let x = {};
+          sut.isNull(x);
+        }, "averment failure: isnull");
+    });
+  });
+
+  describe("#isNotNull()", function() {
+
+    it("pass non null",   function() {
+       let x = {};
+       sut.isNotNull(x);
+    });
+
+    it("fail undefined",   function() {
+      sut.throws(function(){
+          let x = undefined;
+          sut.isNotNull(x);
+        }, "averment failure: isnotnull");
+    });
+
+    it("fail null",   function() {
+      sut.throws(function(){
+          let x = null;
+          sut.isNotNull(x);
+        }, "averment failure: isnotnull");
+    });
+  });
+
+  describe("#isObject()", function() {
+
+    it("pass object",   function() {
+       let x = {};
+       sut.isObject(x);
+    });
+
+    it("fail []",   function() {
+      sut.throws(function(){
+          let x = [];
+          sut.isObject(x);
+        }, "averment failure: isobject");
+    });
+  });
+
+  describe("#isArray()", function() {
+
+    it("pass array",   function() {
+       let x = [];
+       sut.isArray(x);
+    });
+
+    it("fail {}",   function() {
+      sut.throws(function(){
+          let x = {};
+          sut.isArray(x);
+        }, "averment failure: isarray");
+    });
+  });
+
+  describe("#isFunction()", function() {
+
+    it("pass function",   function() {
+       let x = function(){ };
+       sut.isFunction(x);
+    });
+
+    it("fail {}",   function() {
+      sut.throws(function(){
+          let x = {};
+          sut.isFunction(x);
+        }, "averment failure: isfunction");
+    });
+  });
+
+  describe("#isFalse()", function() {
+
+    it("pass false",   function() {
+       let x = false;
+       sut.isFalse(x);
+    });
+
+    it("fail true",   function() {
+      sut.throws(function(){
+          let x = true;
+          sut.isFalse(x);
+        }, "averment failure: isfalse");
+    });
+
+    it("fail undefined",   function() {
+      sut.throws(function(){
+          let x = undefined;
+          sut.isFalse(x);
+        }, "averment failure: isfalse");
+    });
+
+    it("fail null",   function() {
+      sut.throws(function(){
+          let x = null;
+          sut.isFalse(x);
+        }, "averment failure: isfalse");
+    });
+  });
+
+
+  describe("#isTrue()", function() {
+
+    it("pass true",   function() {
+       let x = true;
+       sut.isTrue(x);
+    });
+
+    it("fail false",   function() {
+      sut.throws(function(){
+          let x = false;
+          sut.isTrue(x);
+        }, "averment failure: istrue");
+    });
+
+    it("fail undefined",   function() {
+      sut.throws(function(){
+          let x = undefined;
+          sut.isTrue(x);
+        }, "averment failure: istrue");
+    });
+
+    it("fail null",   function() {
+      sut.throws(function(){
+          let x = null;
+          sut.isTrue(x);
+        }, "averment failure: istrue");
     });
   });
 
