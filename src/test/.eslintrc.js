ccb2da5893aeac83ca8b78f6821a2e5545ef5141
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-04-20 18:13:42 -0400

    latest

--- src/test/.eslintrc.js
+++ src/test/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
     browser: true,
-    es6: false,
+    es6: true,
     mocha: true,
     node: true, 
   },
   rules: {
-    "no-console": "off",
+    "no-console": "off"
   }
 };
