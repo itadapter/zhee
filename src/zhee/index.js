57accb65f842dc4a04b09ca1fcbd964742f9ae68
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-04-21 21:57:31 -0400

    fix build

--- src/zhee/index.js
+++ src/zhee/index.js
@@ -4,6 +4,7 @@ import * as types         from "./types";
 import * as strings       from "./strings";
 import * as localization  from "./localization";
 import * as events        from "./events";
+import * as models        from "./models";
 
 export const CoreConsts   = coreconsts;
 export const Aver         = aver;
@@ -11,3 +12,4 @@ export const Types        = types;
 export const Strings      = strings;
 export const Localization = localization;
 export const Events       = events;
+export const Models       = models;
--- src/zhee/models.js
+++ src/zhee/models.js
@@ -6,7 +6,7 @@ export class ValidationError{
 
   }
 
-  get code ()
+  get code (){ return 0;}
 }
 
 
